aab9058cc6bce9737fe25418df8155dd4a9fed65
Dmitry Kozyrev <dimon3489@mail.ru>
2024-11-30 13:51:47 +0300

    123

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_backend/'
 
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
